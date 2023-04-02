6a11331fb9ae8dfb9d8c48fa99e83279f0964647
diypcfev <username@email.com>
2023-04-02 15:13:51 +0800

    init

---
+++ src/main/java/com/github/makewheels/cfdatabasebackup/CfDatabaseBackupApplication.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.cfdatabasebackup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfDatabaseBackupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CfDatabaseBackupApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/makewheels/cfdatabasebackup/CfDatabaseBackupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.cfdatabasebackup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfDatabaseBackupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
